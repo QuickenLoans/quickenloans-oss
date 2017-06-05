024c26672996316c69c707a41d384b3fd78ad9dc
Joshua T Kalis <kalisjoshua@gmail.com>
2017-06-05 08:20:22 -0400

    add subtree for all-projects repository

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
       from the internal - GitHub Enterprise - repository.
   2. Run a build - `npm run build` - to generate the landing page.
   3. Static resources will be in the `build/` directory.
+
+git remote add all-projects git@git:quickenloans-oss/all-projects.git
+git subtree add --prefix projects-source all-projects master --squash
--- bin/projects.js
+++ bin/projects.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 const https = require('https')
 const path = require('path')
+const yaml = require('js-yaml').safeLoad
+
+const CWD = process.cwd()
 
 const file = str =>
-  fs.readFileSync(path.join(process.cwd(), str), 'utf-8')
+  fs.readFileSync(path.join(CWD, str), 'utf-8')
 
 const args = process.argv
   .reduce((acc, item, indx, ary) => {
@@ -15,63 +18,78 @@ const args = process.argv
   }, {})
 
 function card(project) {
-  if (!project.description || !project.href || !project.title) {
+  const link = project.url && (project.url.project || project.url.repository)
+
+  if (!project.description || !link || !project.title) {
     console.error(project)
-    throw new Error('Projects must have at least, a: title, description, and link (href).')
+    throw new Error('Projects must have at least, a: title, description, and link.')
   }
 
   return args.card
     .replace(/#description/g, project.description)
-    .replace(/#href/g, project.href)
+    .replace(/#href/g, link)
     .replace(/#keywords/g, project.keywords || '')
     .replace(/#logo/g, project.logo || 'http://via.placeholder.com/80x80')
     .replace(/#title/g, project.title)
 }
 
-https.get(process.env.QLOSSPROJECTS, res => {
-  const statusCode = res.statusCode
-  const contentType = res.headers['content-type']
+function projects(subtree = './projects/projects/') {
 
-  let error
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\n` +
-                      `Status Code: ${statusCode}`)
-  } else if (!/^application\/json/.test(contentType)) {
-    error = new Error(`Invalid content-type.\n` +
-                      `Expected application/json but received ${contentType}`)
-  }
+  return fs.readdirSync(path.join(CWD, subtree))
+    .map(entry => yaml(fs.readFileSync(path.join(CWD, subtree, entry), 'utf-8')))
+}
 
-  if (error) {
-    console.error(error.message)
-    // consume response data to free up memory
-    res.resume()
+const page = args.template
+  .replace(/#pagecontent/g, projects().map(card).join('\n'))
 
-    return
-  }
+const dest = path.join(CWD, process.argv.pop())
 
-  res.setEncoding('utf8')
-  let rawData = ''
-  res.on('data', chunk => rawData += chunk)
-  res.on('end', () => {
-    try {
-      const projects = JSON.parse(rawData)
-        // TODO: REMOVE; FOR TESTING PURPOSES ONLY
-        .map(({body: description, title}) => ({
-          description,
-          href: `//github.com/${Math.random().toString(32).slice(2)}`,
-          title,
-        }))
+fs.writeFileSync(dest, page, 'utf-8')
 
-      const page = args.template
-        .replace(/#pagecontent/g, projects.map(card).join('\n'))
-
-      const dest = path.join(process.cwd(), process.argv.pop())
-
-      fs.writeFileSync(dest, page, 'utf-8')
-    } catch (e) {
-      console.error(e.message)
-    }
-  })
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`)
-})
+// https.get(process.env.QLOSSPROJECTS, res => {
+//   const statusCode = res.statusCode
+//   const contentType = res.headers['content-type']
+//
+//   let error
+//   if (statusCode !== 200) {
+//     error = new Error(`Request Failed.\n` +
+//                       `Status Code: ${statusCode}`)
+//   } else if (!/^application\/json/.test(contentType)) {
+//     error = new Error(`Invalid content-type.\n` +
+//                       `Expected application/json but received ${contentType}`)
+//   }
+//
+//   if (error) {
+//     console.error(error.message)
+//     // consume response data to free up memory
+//     res.resume()
+//
+//     return
+//   }
+//
+//   res.setEncoding('utf8')
+//   let rawData = ''
+//   res.on('data', chunk => rawData += chunk)
+//   res.on('end', () => {
+//     try {
+//       const projects = JSON.parse(rawData)
+//         // TODO: REMOVE; FOR TESTING PURPOSES ONLY
+//         .map(({body: description, title}) => ({
+//           description,
+//           href: `//github.com/${Math.random().toString(32).slice(2)}`,
+//           title,
+//         }))
+//
+//       const page = args.template
+//         .replace(/#pagecontent/g, projects.map(card).join('\n'))
+//
+//       const dest = path.join(CWD, process.argv.pop())
+//
+//       fs.writeFileSync(dest, page, 'utf-8')
+//     } catch (e) {
+//       console.error(e.message)
+//     }
+//   })
+// }).on('error', (e) => {
+//   console.error(`Got error: ${e.message}`)
+// })
