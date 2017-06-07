d2d05ec33c86befab302f26970711bc2e58a4004
Joshua T Kalis <kalisjoshua@gmail.com>
2017-06-07 10:59:16 -0400

    update card format

--- bin/projects.js
+++ bin/projects.js
@@ -31,12 +31,15 @@ function card(project) {
     keywords.push(project.language)
   }
 
+  const type = project.type.slice(0, 1).toUpperCase() + project.type.slice(1).toLowerCase()
+
   return args.card
     .replace(/#description/g, project.description)
     .replace(/#href/g, link)
     .replace(/#keywords/g, keywords)
     .replace(/#language-color/g, project.language.replace('#', 'Sharp') || '_')
     .replace(/#language/g, project.language || '')
+    .replace(/#type/g, type || '')
     .replace(/#logo/g, project.logo || 'http://via.placeholder.com/80x80')
     .replace(/#title/g, project.title)
 }
