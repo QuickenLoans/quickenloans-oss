0294655a8318d7ee3d11090fa2f970cd30e8b037
Dean Dreon <deandreon@quickenloans.com>
2017-07-03 14:40:44 -0400

    Merge branch 'master' into ddreon-filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,15 +1,18 @@
 (function(){
 	'use strict';
 
-	let filters = [];
-	let searchTerm = '';
-
 	const projectCards = [].slice.call(document.querySelectorAll('.project--card'), 0);
 	const projectCardsWithInnerText = projectCards.map(cardEl => {return {cardEl: cardEl, cardInnerText: getAllInnerText(cardEl)}});
 
 	const filterInput = document.querySelector('#filter');
 	const quickFilters = document.querySelector('.quick-filters');
 	const projectsContainer = document.querySelector('.projects-container');
+
+	let filters = getParameterByName('filters') ? getParameterByName('filters').split(',') : [];
+	let searchTerm = getParameterByName('searchTerm') || '';
+	filterInput.value = searchTerm;
+	assignFilterClasses();
+	assignSelectedClassToFilterItems();
 
 	filterInput.addEventListener('input', debounce(enteredSearchTerm, 300));
 	quickFilters.addEventListener('click', clickedFilterTerm)
@@ -46,16 +49,30 @@
 	function enteredSearchTerm(e) {
 		searchTerm = e.target.value;
 		assignFilterClasses();
+	    updateQueryParams(filters, searchTerm);
 	}
 
 	function clickedFilterTerm(e) {
+	    if(e.target.tagName !== 'LI') return;
 		const filterText = e.target.innerText;
 		const filterIndex = filters.findIndex(text=>text===filterText);
 		(filterIndex > -1)
 			? filters.splice(filterIndex, 1)
 			: filters.push(filterText);
 		assignFilterClasses();
+	    updateQueryParams(filters, searchTerm);
+	    assignSelectedClassToFilterItems();
 	}
+
+    function assignSelectedClassToFilterItems() {
+        const filterItems = quickFilters.querySelectorAll('li');
+        filterItems.forEach(filterItem => {
+            (filters.find(filterText=>filterItem.innerText === filterText))
+                ? filterItem.classList.add('selected')
+                : filterItem.classList.remove('selected');
+
+        });
+    }
 
 	function assignFilterClasses() {
 		const shownCards = projectCardsWithInnerText
@@ -77,4 +94,23 @@
 			: cardEl.classList.add('filtered-out');
 		return show;
 	}
+
+	function updateQueryParams(filters, searchTerm) {
+	    const filtersParam = filters.length ? `filters=${filters.join(',')}` : '';
+	    const searchTermParam = searchTerm ? `searchTerm=${searchTerm}` : '';
+	    const bothExist = !!(filtersParam && searchTermParam);
+	    const eitherExist = !!(filtersParam || searchTermParam);
+        history
+        .replaceState({}, '', `?${filtersParam}${bothExist ? '&' : ''}${searchTermParam}`);
+	}
+
+	function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", 'i'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 })();
