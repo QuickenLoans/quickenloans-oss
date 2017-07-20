1eb21b6ad0503b9c51cd26d1c73dda8b67d4c4b7
Joshua T Kalis <kalisjoshua@gmail.com>
2017-07-20 17:17:58 -0400

    linting fixes

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,116 +1,158 @@
-(function(){
-	'use strict';
+(function iife() {
+  // 'use strict';
 
-	const projectCards = [].slice.call(document.querySelectorAll('.project--card'), 0);
-	const projectCardsWithInnerText = projectCards.map(cardEl => {return {cardEl: cardEl, cardInnerText: getAllInnerText(cardEl)}});
+  const projectCardsWithInnerText = [].slice
+    .call(document.querySelectorAll('.project--card'), 0)
+    .map(cardEl => ({
+      cardEl,
+      cardInnerText: getAllInnerText(cardEl),
+    }));
 
-	const filterInput = document.querySelector('#filter');
-	const quickFilters = document.querySelector('.quick-filters');
-	const projectsContainer = document.querySelector('.projects-container');
+  const filterInput = document.querySelector('#filter');
+  const quickFilters = document.querySelector('.quick-filters');
+  const projectsContainer = document.querySelector('.projects-container');
 
-	let filters = getParameterByName('filters') ? getParameterByName('filters').split(',') : [];
-	let searchTerm = getParameterByName('searchTerm') || '';
-	filterInput.value = searchTerm;
-	assignFilterClasses();
-	assignSelectedClassToFilterItems();
+  const filters = getParameterByName('filters')
+    ? getParameterByName('filters').split(',')
+    : [];
 
-	filterInput.addEventListener('input', debounce(enteredSearchTerm, 300));
-	quickFilters.addEventListener('click', clickedFilterTerm)
+  let searchTerm = getParameterByName('searchTerm') || '';
+  filterInput.value = searchTerm;
 
-	function debounce(func, wait, immediate) {
-		let timeout;
-		return function() {
-			const context = this,
-						args = arguments;
-			let later = function() {
-				timeout = null;
-				if ( !immediate ) {
-					func.apply(context, args);
-				}
-			};
-			const callNow = immediate && !timeout;
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait || 200);
-			if ( callNow ) {
-				func.apply(context, args);
-			}
-		}
-	};
+  assignFilterClasses();
+  assignSelectedClassToFilterItems();
 
-	function getAllInnerText(el) {
-		const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
-		let n, ary = [];
-		while(n = walker.nextNode()) {
-			ary.push(n.textContent);
-		}
-		return ary.join(' ').replace(/\s\s+/g, ' ');
-	}
+  filterInput.addEventListener('input', debounce(enteredSearchTerm, 300));
+  quickFilters.addEventListener('click', clickedFilterTerm);
 
-	function enteredSearchTerm(e) {
-		searchTerm = e.target.value;
-		assignFilterClasses();
-	    updateQueryParams(filters, searchTerm);
-	}
+  function debounce(func, wait, immediate) {
+    let timeout;
 
-	function clickedFilterTerm(e) {
-	    if(e.target.tagName !== 'LI') return;
-		const filterText = e.target.innerText;
-		const filterIndex = filters.findIndex(text=>text===filterText);
-		(filterIndex > -1)
-			? filters.splice(filterIndex, 1)
-			: filters.push(filterText);
-		assignFilterClasses();
-	    updateQueryParams(filters, searchTerm);
-	    assignSelectedClassToFilterItems();
-	}
+    function bounce(...args) {
+      const context = this;
 
-    function assignSelectedClassToFilterItems() {
-        const filterItems = quickFilters.querySelectorAll('li');
-        filterItems.forEach(filterItem => {
-            (filters.find(filterText=>filterItem.innerText === filterText))
-                ? filterItem.classList.add('selected')
-                : filterItem.classList.remove('selected');
+      function later() {
+        timeout = null;
 
-        });
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      }
+
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait || 200);
+
+      if (callNow) {
+        func.apply(context, args);
+      }
     }
 
-	function assignFilterClasses() {
-		const shownCards = projectCardsWithInnerText
-											.filter(assignFilteredOutClass);
-		(shownCards.length === 0)
-			? projectsContainer.classList.add('no-cards-shown')
-			: projectsContainer.classList.remove('no-cards-shown');
-	}
+    return bounce;
+  }
 
-	function assignFilteredOutClass({cardEl, cardInnerText}){
-		const searchRegex = new RegExp(
-								filters.map(text=>text).join('|')
-								+ (filters.length > 0 && searchTerm !== '' && '|' || '')
-								+ searchTerm
-								, 'gi');
-		const show = searchRegex.test(cardInnerText) || (filters.length === 0 && searchTerm === '');
-		show
-			? cardEl.classList.remove('filtered-out')
-			: cardEl.classList.add('filtered-out');
-		return show;
-	}
+  function getAllInnerText(el) {
+    const walker = document
+      .createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
 
-	function updateQueryParams(filters, searchTerm) {
-	    const filtersParam = filters.length ? `filters=${filters.join(',')}` : '';
-	    const searchTermParam = searchTerm ? `searchTerm=${searchTerm}` : '';
-	    const bothExist = !!(filtersParam && searchTermParam);
-	    const eitherExist = !!(filtersParam || searchTermParam);
-        history
-        .replaceState({}, '', `?${filtersParam}${bothExist ? '&' : ''}${searchTermParam}`);
-	}
+    let n = walker.nextNode();
+    const ary = [];
 
-	function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", 'i'),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    while (n) {
+      ary.push(n.textContent);
+      n = walker.nextNode();
     }
-})();
+
+    return ary.join(' ').replace(/\s\s+/g, ' ');
+  }
+
+  function enteredSearchTerm(e) {
+    searchTerm = e.target.value;
+    assignFilterClasses();
+    updateQueryParams(filters, searchTerm);
+  }
+
+  function clickedFilterTerm(e) {
+    if (e.target.tagName !== 'LI') return;
+
+    const filterText = e.target.innerText;
+    const filterIndex = filters.findIndex(text => text === filterText);
+
+    if (filterIndex > -1) {
+      filters.splice(filterIndex, 1);
+    } else {
+      filters.push(filterText);
+    }
+
+    assignFilterClasses();
+    updateQueryParams(filters, searchTerm);
+    assignSelectedClassToFilterItems();
+  }
+
+  function assignSelectedClassToFilterItems() {
+    const filterItems = quickFilters.querySelectorAll('li');
+    filterItems.forEach((filterItem) => {
+      const findFn = filterText =>
+        filterItem.innerText === filterText;
+
+      const method = filters.find(findFn)
+        ? 'add'
+        : 'remove';
+
+      filterItem.classList[method]('selected');
+    });
+  }
+
+  function assignFilterClasses() {
+    const shownCards = projectCardsWithInnerText
+      .filter(assignFilteredOutClass);
+
+    const method = shownCards.length === 0
+      ? 'add'
+      : 'remove';
+
+    projectsContainer.classList[method]('no-cards-shown');
+  }
+
+  function assignFilteredOutClass({ cardEl, cardInnerText }) {
+    const searchRegex = new RegExp(
+      filters.map(text => text).join('|')
+      + ((filters.length > 0 && searchTerm !== '' && '|') || '')
+      + searchTerm
+      , 'gi');
+
+    const show = searchRegex.test(cardInnerText) ||
+      (filters.length === 0 && searchTerm === '');
+
+    cardEl.classList[show ? 'remove' : 'add']('filtered-out');
+
+    return show;
+  }
+
+  function updateQueryParams(strings, terms) {
+    const filtersParam = strings.length ? `filters=${strings.join(',')}` : '';
+    const searchTermParam = terms ? `searchTerm=${terms}` : '';
+
+    history
+      .replaceState({}, '', `?${[filtersParam, searchTermParam].join('&')}`);
+  }
+
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+
+    name = name.replace(/[[\]]/g, '\\$&');
+
+    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`, 'i');
+    const results = regex.exec(url);
+
+    if (!results) {
+      return null;
+    }
+
+    if (!results[2]) {
+      return '';
+    }
+
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+}());
